27311aafb67649b2743f2e814347be242885c67c
Ben754444 <50147798+Ben754444@users.noreply.github.com>
2025-12-21 16:47:06 +0000

    feat: change to forwardauth

--- main.go
+++ main.go
@@ -8,18 +8,56 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/go-ldap/ldap/v3"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/net/html/charset"
 )
 
 type ServiceResponse struct {
 	User string `xml:"authenticationSuccess>user"`
+}
+
+func isInLdapGroups(user string, ldapGroups []string, conn *ldap.Conn) bool {
+	if len(ldapGroups) == 0 {
+		return true
+	}
+
+	slog.Debug("Checking LDAP groups for user:", slog.String("user", user), slog.String("groups", strings.Join(ldapGroups, ", ")))
+
+	searchRequest := ldap.NewSearchRequest("ou=groups,o=bath.ac.uk", ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false, fmt.Sprintf("(&(objectClass=groupOfNames)(member=uid=%s,ou=people,o=bath.ac.uk))", user), []string{"cn"}, nil)
+
+	sr, err := conn.Search(searchRequest)
+	if err != nil {
+		if err.(*ldap.Error).ResultCode == ldap.ErrorNetwork {
+			slog.Debug("LDAP connection lost, reconnecting...")
+			var err error
+			conn, err = ldap.DialURL(os.Getenv("LDAP_URL"))
+			if err != nil {
+				slog.Debug("Failed to reconnect to LDAP server:", slog.String("error", err.Error()))
+				return false
+			}
+			return isInLdapGroups(user, ldapGroups, conn)
+		}
+		slog.Debug("LDAP search error:", slog.String("error", err.Error()))
+		return false
+
+	}
+
+	for _, entry := range sr.Entries {
+		groupName := entry.GetAttributeValue("cn")
+		for _, g := range ldapGroups {
+			if g == groupName {
+				return true
+			}
+		}
+	}
+	return false
+
 }
 
 func isWhitelisted(user string, whitelist []string) bool {
@@ -34,9 +72,10 @@ func isWhitelisted(user string, whitelist []string) bool {
 	return false
 }
 
-func genToken(user string, hmacSecret []byte) string {
+func genToken(user string, host string, hmacSecret []byte) string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user": user,
+		"host": host,
 		"exp":  jwt.NewNumericDate(time.Now().Add(6 * time.Hour)),
 	})
 	tokenString, err := token.SignedString(hmacSecret)
@@ -54,17 +93,6 @@ func main() {
 		println("Invalid CAS_URL:", err.Error())
 		return
 	}
-	appUrl, err := url.ParseRequestURI(os.Getenv("APP_URL"))
-	if err != nil {
-		println("Invalid APP_URL:", err.Error())
-		return
-	}
-	// prevents other hosts from pointing to it
-	proxyUrl, err := url.ParseRequestURI(os.Getenv("PROXY_URL"))
-	if err != nil {
-		println("Invalid PROXY_URL:", err.Error())
-		return
-	}
 
 	hmacSecret := []byte(os.Getenv("JWT_SECRET"))
 	if len(hmacSecret) == 0 {
@@ -72,10 +100,15 @@ func main() {
 		return
 	}
 
-	whitelist := os.Getenv("WHITELISTED_USERS")
-	var whitelistArr []string
-	if whitelist != "" {
-		whitelistArr = strings.Split(whitelist, ",")
+	ldapUrl := os.Getenv("LDAP_URL")
+
+	var conn *ldap.Conn
+	if len(ldapUrl) > 0 {
+		conn, err = ldap.DialURL(ldapUrl)
+		if err != nil {
+			println("Failed to connect to LDAP server:", err.Error())
+			return
+		}
 	}
 
 	port := os.Getenv("PORT")
@@ -83,29 +116,28 @@ func main() {
 		port = "8080"
 	}
 
-	errorFile := os.Getenv("ERROR_FILE")
-	var errorData []byte
-	if errorFile != "" {
-		_, err := os.Stat(errorFile)
-		if os.IsNotExist(err) {
-			println("ERROR_FILE does not exist:", errorFile)
-			return
-		}
-
-		if err != nil {
-			println("Failed to read ERROR_FILE:", err.Error())
-			return
-		}
-		errorData, err = os.ReadFile(errorFile)
-
-	}
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var whitelistArr []string
+		if len(r.URL.Query().Get("whitelist")) != 0 {
+			whitelistArr = strings.Split(r.URL.Query().Get("whitelist"), ",")
+		}
+
+		var ldapGroups []string
+		if len(r.URL.Query().Get("ldapgroups")) != 0 {
+			ldapGroups = strings.Split(r.URL.Query().Get("ldapgroups"), ",")
+		}
+
+		originalUrl, err := url.ParseRequestURI(r.Header.Get("X-Forwarded-Uri"))
+
+		if err == nil {
+			r.URL = originalUrl
+		} else {
+			println("Failed to parse X-Forwarded-Url:", err.Error())
+		}
+		println(r.Header.Get("X-Forwarded-Proto"))
 		newUrl := &url.URL{
-			Scheme:   proxyUrl.Scheme,
-			Opaque:   proxyUrl.Opaque,
-			Host:     proxyUrl.Host,
-			User:     proxyUrl.User,
+			Scheme:   r.Header.Get("X-Forwarded-Proto"),
+			Host:     r.Header.Get("X-Forwarded-Host"),
 			Path:     r.URL.Path,
 			RawQuery: r.URL.RawQuery,
 			Fragment: r.URL.Fragment,
@@ -117,7 +149,7 @@ func main() {
 				return hmacSecret, nil
 			}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
 
-			if token == nil || !token.Valid {
+			if token == nil || !token.Valid || token.Claims.(jwt.MapClaims)["host"].(string) != newUrl.Host {
 				http.SetCookie(w, &http.Cookie{
 					Name:   "session_id",
 					MaxAge: -1,
@@ -130,48 +162,48 @@ func main() {
 
 			user := token.Claims.(jwt.MapClaims)["user"].(string)
 
-			remaining := jwt.NewNumericDate(time.Now()).Unix() - int64(token.Claims.(jwt.MapClaims)["exp"].(float64))
+			remaining := int64(token.Claims.(jwt.MapClaims)["exp"].(float64)) - jwt.NewNumericDate(time.Now()).Unix()
 
+			// TODO infinite session length
 			if remaining < 5*60*60 {
+				if !isWhitelisted(user, whitelistArr) {
+					slog.Debug("User not whitelisted:", slog.String("user", user))
+					http.SetCookie(w, &http.Cookie{
+						Name:   "session_id",
+						Value:  "",
+						MaxAge: -1,
+					})
+					http.Error(w, "access denied", http.StatusForbidden)
+					return
+				}
+
+				if len(ldapUrl) > 0 {
+					if !isInLdapGroups(user, ldapGroups, conn) {
+						slog.Debug("User not in LDAP groups:", slog.String("user", user))
+						// could overload ldap if a valid token cannot be refreshed
+						http.SetCookie(w, &http.Cookie{
+							Name:   "session_id",
+							Value:  "",
+							MaxAge: -1,
+						})
+						http.Error(w, "access denied (ldap)", http.StatusForbidden)
+						return
+					}
+				}
+
 				http.SetCookie(w, &http.Cookie{
 					Name:   "session_id",
-					Value:  genToken(user, hmacSecret),
+					Value:  genToken(user, newUrl.Host, hmacSecret),
 					MaxAge: 6 * 60 * 60,
 				})
+
+				http.Redirect(w, r, newUrl.String(), http.StatusFound)
 			}
 
-			if !isWhitelisted(user, whitelistArr) {
-				slog.Debug("User not whitelisted:", slog.String("user", user))
-				http.Error(w, "access denied", http.StatusForbidden)
-				return
-			}
-
-			proxy := httputil.ReverseProxy{
-				Director: func(req *http.Request) {
-					req.URL.Scheme = appUrl.Scheme
-					req.URL.Host = appUrl.Host
-					req.URL.Path = appUrl.Path + r.URL.Path
-					req.Host = appUrl.Host
-					for k, v := range r.Header {
-						req.Header[k] = v
-					}
-					req.Header.Set("X-User", user)
-				},
-				ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
-					slog.Debug("Proxy error:", slog.String("error", err.Error()))
-					// read and send html file
-					if errorData != nil {
-						w.Header().Set("Content-Type", "text/html")
-						w.WriteHeader(http.StatusBadGateway)
-						w.Write(errorData)
-					} else {
-						http.Error(w, "upstream communication error", http.StatusBadGateway)
-					}
-
-				},
-			}
-			proxy.ServeHTTP(w, r)
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("X-User", user)
 			return
+
 		}
 
 		if r.URL.Query().Get("ticket") != "" {
@@ -180,18 +212,13 @@ func main() {
 			query := r.URL.Query()
 			query.Del("ticket")
 			rawQuery := query.Encode()
-			// r.URL doesnt have the fucking scheme (or probably host) guessing cuz of http.handleFunc starting at /.
-			// go back to proxy_url maybe but that was beong weird
 			newUrl := &url.URL{
-				Scheme:   proxyUrl.Scheme,
-				Opaque:   proxyUrl.Opaque,
-				Host:     proxyUrl.Host,
-				User:     proxyUrl.User,
+				Scheme:   r.Header.Get("X-Forwarded-Proto"),
+				Host:     r.Header.Get("X-Forwarded-Host"),
 				Path:     r.URL.Path,
 				RawQuery: rawQuery,
 				Fragment: r.URL.Fragment,
 			}
-			println(newUrl.String())
 			serviceValidation, err := http.Get(casUrl.String() + "/serviceValidate?service=" + url.QueryEscape(newUrl.String()) + "&ticket=" + ticket)
 			if err != nil || serviceValidation.StatusCode != http.StatusOK {
 				slog.Debug("Failed to validate CAS ticket:", slog.String("error", err.Error()))
@@ -226,29 +253,35 @@ func main() {
 				return
 			}
 
+			if !isWhitelisted(serviceResponse.User, whitelistArr) {
+				slog.Debug("User not whitelisted:", slog.String("user", serviceResponse.User))
+				http.Error(w, "access denied", http.StatusForbidden)
+				return
+			}
+
+			if len(ldapUrl) > 0 {
+				if !isInLdapGroups(serviceResponse.User, ldapGroups, conn) {
+					slog.Debug("User not in LDAP groups:", slog.String("user", serviceResponse.User))
+					http.Error(w, "access denied (ldap)", http.StatusForbidden)
+					return
+				}
+			}
+
 			http.SetCookie(w, &http.Cookie{
 				Name:   "session_id",
-				Value:  genToken(serviceResponse.User, hmacSecret),
+				Value:  genToken(serviceResponse.User, newUrl.Host, hmacSecret),
 				MaxAge: 6 * 60 * 60,
 			})
 
 			// remove ticket query
-			originalUrl := r.URL
-			q := originalUrl.Query()
-			q.Del("ticket")
-			originalUrl.RawQuery = q.Encode()
-			http.Redirect(w, r, proxyUrl.String()+originalUrl.String(), http.StatusFound)
-
+			http.Redirect(w, r, newUrl.String(), http.StatusFound)
 			return
 		}
-
 		http.Redirect(w, r, casUrl.String()+"/login?service="+url.QueryEscape(newUrl.String()), http.StatusFound)
 
 	})
 
 	println("CAS URL:", casUrl.String())
-	println("App URL:", appUrl.String())
-	println("Proxy URL:", proxyUrl.String())
 
 	println("Starting proxy server on port", port)
 	err = http.ListenAndServe(":"+port, nil)
